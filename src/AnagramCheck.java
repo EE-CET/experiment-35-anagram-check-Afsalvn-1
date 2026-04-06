fa8597924fd183649d0bcf4b1123391ef40dc318
Afsalvn <147943587+Afsalvn@users.noreply.github.com>
2026-04-06 17:06:22 +0530

    Update AnagramCheck.java

--- src/AnagramCheck.java
+++ src/AnagramCheck.java
@@ -1,10 +1,22 @@
+
+import java.util.Arrays;
+
 public class AnagramCheck {
-   
-        // TODO: Read the two strings
-        // TODO: Convert both to lowercase (to handle case-insensitivity)
-        // TODO: Check if lengths are equal. If not, print "false" and return.
-        // TODO: Convert strings to char arrays
-        // TODO: Sort both char arrays
-        // TODO: Compare the sorted arrays. Print "true" if equal, else "false".
-  
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        if (sc.hasNextLine()) {
+            String s1 = sc.nextLine().toLowerCase();
+            String s2 = sc.nextLine().toLowerCase();
+            
+            // Convert to char arrays and sort them
+            char[] arr1 = s1.toCharArray();
+            char[] arr2 = s2.toCharArray();
+            Arrays.sort(arr1);
+            Arrays.sort(arr2);
+            
+            // If the sorted arrays are identical, they are anagrams
+            System.out.println(Arrays.equals(arr1, arr2));
+        }
+        sc.close();
+    }
 }
